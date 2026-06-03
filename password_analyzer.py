11f17fcb5ad1b0f8db2095f542c5559491c2c227
25it111-afk <25it111@rmkec.ac.in>
2026-06-03 11:36:08 +0530

    Add files via upload

---
+++ password_analyzer.py
@@ -0,0 +1,34 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+
+    if re.search(r"[a-z]", password):
+        score += 1
+
+    if re.search(r"\d", password):
+        score += 1
+
+    if re.search(r"[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>/?]", password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
+
+password = input("Enter a password: ")
+strength = check_password_strength(password)
+
+print("Password Strength:", strength)
+
+if strength != "Strong":
+    print("Suggestion: Use uppercase, lowercase, numbers, and special characters.")
